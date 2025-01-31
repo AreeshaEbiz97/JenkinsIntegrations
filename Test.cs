ce346a10b4670df05d114f26d79622e8bf08969e
Areesha Raees <areesha.ahmed@e-bizsoft.com>
2025-01-31 11:55:01 +0500

    jenkins integration commit

--- Test.cs
+++ Test.cs
@@ -1,3 +1,4 @@
+
 using PlaywrightEbizPOM;  // Ensure this namespace is included
 using NUnit.Framework;
 using System.Threading.Tasks;
@@ -17,11 +18,11 @@ public class Tests : BaseClass
         var loginPage = new LoginPage(Page);
         await loginPage.LoginAsync("areesha.ahmedebizsoft3@gmail.com", "Aa1234567");
 
-         // Navigate using menu image and Home link
+         // Navigate using menu image and Home link add test details 
         await Page.Locator("#menuimage").ClickAsync();
         await Page.Locator("a:has-text('Home')").ClickAsync();
 
          var isProfileIconVisible = await Page.IsVisibleAsync("#profileIcon");
         Assert.IsTrue(isProfileIconVisible, "Login failed. Profile icon not visible.");
     }
-} 
+}  
